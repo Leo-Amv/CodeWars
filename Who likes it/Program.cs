4d9d085cb320f713d0449f29e6f12acabdd002a7
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-02 20:24:35 +0500

    Who likes it? completed

--- Who likes it/Program.cs
+++ Who likes it/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public static class Kata
+{
+    public static string Likes(string[] name)
+    {
+        string likes = "";
+        if (name.Length == 0) likes = ("no one likes this");
+        if (name.Length == 1) likes = ($"{name[0]} likes this");
+        if (name.Length == 2) likes = ($"{name[0]} and {name[1]} like this");
+        if (name.Length == 3) likes = ($"{name[0]}, {name[1]} and {name[2]} like this");
+        if (name.Length > 3) likes = ($"{name[0]}, {name[1]} and {name.Length - 2} others like this");
+        return likes;
+        throw new NotImplementedException();
+    }
+}
