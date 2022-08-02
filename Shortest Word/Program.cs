a4481b20a143d61022e9cd73048fd4121b9707b7
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-02 19:58:39 +0500

    Shortest Word completed

--- Shortest Word/Program.cs
+++ Shortest Word/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Kata
+{
+    public static int FindShort(string s)
+    {
+        string[] words = s.Split(" ");
+        string shortest = words[0];
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length <= shortest.Length) shortest = words[i];
+        }
+        return shortest.Length;
+    }
+}
