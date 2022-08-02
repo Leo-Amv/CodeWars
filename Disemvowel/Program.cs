20b663b942a6780edd9f65f0edd3ec336729a1bc
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-02 19:06:59 +0500

    Disemvowel completed!

--- Disemvowel/Program.cs
+++ Disemvowel/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Kata
+{
+    public static string Disemvowel(string str)
+    {
+        str = Regex.Replace(str, "[AaEeOoiIuU]", "");
+        return str;
+    }
+}
