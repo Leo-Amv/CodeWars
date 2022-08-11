3a7bfce3a8fc6561c4d319bb39c474b163b0932d
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-11 23:00:44 +0500

    RGB To Hex Conversion completed!

---
+++ RGB To Hex Conversion/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Kata
+{
+    public static string Rgb(int r, int g, int b)
+    {
+        if (r < 0) r = 0;
+        if (g < 0) g = 0;
+        if (b < 0) b = 0;
+        if (r > 255) r = 255;
+        if (g > 255) g = 255;
+        if (b > 255) b = 255;
+        return $"{r.ToString("X2").ToUpper()}{g.ToString("X2").ToUpper()}{b.ToString("X2").ToUpper()}";
+    }
+    public static void Main()
+    {
+        Console.WriteLine(Rgb(144, 195, 212));
+    }
+}
