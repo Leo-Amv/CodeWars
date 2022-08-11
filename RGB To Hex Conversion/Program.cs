c519ed7ccc1d1c5a57b016d1bfb650669ca36181
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-11 23:09:15 +0500

    RGB To Hex Conversion new solution added

--- RGB To Hex Conversion/Program.cs
+++ RGB To Hex Conversion/Program.cs
@@ -2,13 +2,7 @@
 {
     public static string Rgb(int r, int g, int b)
     {
-        if (r < 0) r = 0;
-        if (g < 0) g = 0;
-        if (b < 0) b = 0;
-        if (r > 255) r = 255;
-        if (g > 255) g = 255;
-        if (b > 255) b = 255;
-        return $"{r.ToString("X2").ToUpper()}{g.ToString("X2").ToUpper()}{b.ToString("X2").ToUpper()}";
+        return Math.Clamp(r, 0, 255).ToString("X2") + Math.Clamp(g, 0, 255).ToString("X2") + Math.Clamp(b, 0, 255).ToString("X2");
     }
     public static void Main()
     {
