d65fb177a9b8c0835f1abe1975338f5b7c786f3d
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-17 23:58:32 +0500

    Valid Parentheses completed !

--- Valid Parentheses/Program.cs
+++ Valid Parentheses/Program.cs
@@ -3,41 +3,23 @@
     public static bool ValidParentheses(string input)
     {
         int n = 0;
-        bool x = false;
-        if (input == "" || String.IsNullOrEmpty(input)) return true;
-        char[] result = input.ToCharArray();
-        char[] temp = new char[result.Length];
-        int size = 0;
-        for (int i = 0; i < result.Length; i++)
+        foreach (char t in input)
         {
-            if (result[i] == '(' || result[i] == ')')
-            {
-                temp[size] = result[i];
-                size++;
-            }
-        }
-        Array.Resize(ref temp, size);
-        if (temp[0] == ')') return false;
-        if (temp[temp.Length - 1] == '(') return false;
-        for (int i = 0; i < temp.Length; i++)
-        {
-            if (temp[i] == '(')
+            if (t == '(')
             {
                 n++;
-                x = true;
             }
-            if (temp[i] == ')')
+            else if (t == ')')
             {
                 n--;
-                x = true;
+
+                if (n < 0)
+                {
+                    return false;
+                }
             }
-            if (n == -1) return false;
         }
-        if (n == 0 && x == true) return true;
-        else return false;
-    }
-    public static void Main()
-    {
-        System.Console.WriteLine(ValidParentheses("( )) (( )"));
+
+        return n == 0;
     }
 }
