8c917e026389a352cbc08c3754da7ec7ae05fc31
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-17 23:52:07 +0500

    Test 2 Solution added

--- Valid Parentheses/Program.cs
+++ Valid Parentheses/Program.cs
@@ -3,19 +3,41 @@
     public static bool ValidParentheses(string input)
     {
         int n = 0;
-        char[] temp = input.ToCharArray();
+        bool x = false;
+        if (input == "" || String.IsNullOrEmpty(input)) return true;
+        char[] result = input.ToCharArray();
+        char[] temp = new char[result.Length];
+        int size = 0;
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (result[i] == '(' || result[i] == ')')
+            {
+                temp[size] = result[i];
+                size++;
+            }
+        }
+        Array.Resize(ref temp, size);
         if (temp[0] == ')') return false;
+        if (temp[temp.Length - 1] == '(') return false;
         for (int i = 0; i < temp.Length; i++)
         {
-            if (temp[i] == '(') n++;
-            if (temp[i] == ')') n--;
+            if (temp[i] == '(')
+            {
+                n++;
+                x = true;
+            }
+            if (temp[i] == ')')
+            {
+                n--;
+                x = true;
+            }
             if (n == -1) return false;
         }
-        if (n == 0) return true;
+        if (n == 0 && x == true) return true;
         else return false;
     }
     public static void Main()
     {
-        System.Console.WriteLine(ValidParentheses("(())((()())())"));
+        System.Console.WriteLine(ValidParentheses("( )) (( )"));
     }
 }
