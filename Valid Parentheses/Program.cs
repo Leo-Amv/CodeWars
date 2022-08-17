5d3d07fcbdf35c27f03a1f5d4901d6988ab3f509
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-17 22:49:47 +0500

    Test solution added

--- Valid Parentheses/Program.cs
+++ Valid Parentheses/Program.cs
@@ -2,7 +2,20 @@
 {
     public static bool ValidParentheses(string input)
     {
-        // Your code here
-        return false;
+        int n = 0;
+        char[] temp = input.ToCharArray();
+        if (temp[0] == ')') return false;
+        for (int i = 0; i < temp.Length; i++)
+        {
+            if (temp[i] == '(') n++;
+            if (temp[i] == ')') n--;
+            if (n == -1) return false;
+        }
+        if (n == 0) return true;
+        else return false;
+    }
+    public static void Main()
+    {
+        System.Console.WriteLine(ValidParentheses("(())((()())())"));
     }
 }
