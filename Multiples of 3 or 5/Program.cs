2c750763910525ddba65ac7319617b2305ab9e61
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-18 23:25:51 +0500

    Test solution added

--- Multiples of 3 or 5/Program.cs
+++ Multiples of 3 or 5/Program.cs
@@ -2,8 +2,16 @@
 {
     public static int Solution(int value)
     {
-        // Magic Happens
+        if (value < 0) return 0;
         int sum = 0;
-
+        for (int i = 0; i < value; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0) sum += i;
+        }
+        return sum;
+    }
+    public static void Main()
+    {
+        Console.WriteLine(Solution(10));
     }
 }
