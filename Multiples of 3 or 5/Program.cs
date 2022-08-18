27de6f1ffcbea6c16ba88a6275cdc887dd16fb9e
Leo_Amv <leonidamvrosov@yandex.ru>
2022-08-18 23:29:17 +0500

    Multiples of 3 or 5 completed !

--- Multiples of 3 or 5/Program.cs
+++ Multiples of 3 or 5/Program.cs
@@ -2,16 +2,10 @@
 {
     public static int Solution(int value)
     {
-        if (value < 0) return 0;
-        int sum = 0;
-        for (int i = 0; i < value; i++)
-        {
-            if (i % 3 == 0 || i % 5 == 0) sum += i;
-        }
-        return sum;
+        return Enumerable.Range(0, value).Where(e => e % 3 == 0 || e % 5 == 0).Sum();
     }
     public static void Main()
     {
-        Console.WriteLine(Solution(10));
+        Console.WriteLine(Solution(100));
     }
 }
